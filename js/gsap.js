acbbf918d7194630fc17dd9cefeeeae1eb17e1b5
hugetuna <113012817+hugetuna@users.noreply.github.com>
2023-12-22 17:31:30 +0800

    Add files via upload

--- js/gsap.js
+++ js/gsap.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
     gsap.registerPlugin(ScrollTrigger);
+    //漢堡動畫
+    let open = 0;//0=close,1=open
+    $('#hamburger').click(
+        function () {
+            $(this).toggleClass('is-active');
+            $(".nav").slideToggle(500);
+            open = 1 - open;
+        });
+    $('.nav-li').click(
+        function () {
+            if (open == 1) {
+                $('#hamburger').toggleClass('is-active');
+                $(".nav").slideToggle(500);
+                open = 1 - open;
+            }
+        });
     //簡介淡入動畫
     let anime_introbox1_scroll = gsap.timeline({
         scrollTrigger: {
@@ -46,10 +62,10 @@ $(document).ready(function () {
         },
     });
 
-    anime_introbox1_scroll.from("#intro-box-1", 1.5, { opacity: 0, y: 100 });
+    anime_introbox1_scroll.from("#intro-box-1", 1.5, { opacity: 0, y: 200, ease: "power3.out" });
     anime_introbox1_scroll.from(".totop", 0.5, { opacity: 0 });
-    anime_introbox2_scroll.from("#intro-box-2", 1.5, { opacity: 0, y: 100 });
-    anime_introbox3_scroll.from("#intro-box-3", 1.5, { opacity: 0, y: 100 });
+    anime_introbox2_scroll.from("#intro-box-2", 1.5, { opacity: 0, y: 200, ease: "power3.out" });
+    anime_introbox3_scroll.from("#intro-box-3", 1.5, { opacity: 0, y: 200, ease: "power3.out" });
 
     anime_nav_scroll.from("#nav", 0.5, { opacity: 0 });
 
@@ -57,10 +73,10 @@ $(document).ready(function () {
     //角色介紹區調控
     $('#select-pic > .select-picbox').hover(
         function () {//hover-in
-            gsap.to(this, 0.5, { scaleX: 1.2, scaleY: 1.2 })
+            gsap.to(this, 0.5, { scaleX: 1.2, scaleY: 1.2 });
         },
         function () {//hover-out
-            gsap.to(this, 0.5, { scaleX: 1, scaleY: 1 })
+            gsap.to(this, 0.5, { scaleX: 1, scaleY: 1 });
         }
     );
     $('#select-pic > .select-picbox').click(
@@ -70,4 +86,17 @@ $(document).ready(function () {
         }
     );
 
+    //支持我們調控
+    let anime_supbar_scroll = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#sup-block",
+            pin: false,
+            start: "center bottom",
+            end: "bottom bottom",
+            scrub: false,
+            markers: true,
+            id: "anime-supbar",
+        },
+    });
+    anime_supbar_scroll.to(".sup-bar", 2, { height: 675 });
 });
